f31b807b6310f61bdc57cd669cc0c6cb9b292f9b
Meha <mixiadami@gmail.com>
2025-06-08 21:51:51 +0200

    JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is fun");
 console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log("23");
+let firstname = "Jonas";
+console.log(firstname);
