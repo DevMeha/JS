df0602d93248683c9fcef366320ec9d47f39cc24
Meha <mixiadami@gmail.com>
2025-06-09 23:06:49 +0200

    JS

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun");
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun");
+// console.log(40 + 8 + 23 - 10);
 
-console.log("jonas");
-console.log("23");
-let firstname = "Jonas";
-console.log(firstname);
+// console.log("jonas");
+// console.log("23");
+// let firstname = "Jonas";
+// console.log(firstname);
+
+// true;
+// console.log(true);
+let age = 30;
+age = 31;
+const birthYear = 1991;
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+let x = 10 + 5;
+x += 10;
+console.log(x);
