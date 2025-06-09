eb64c55be4d33fe7d6f68db222ba81009e1bce8a
Meha <mixiadami@gmail.com>
2025-06-09 23:49:48 +0200

    value

--- script.js
+++ script.js
@@ -9,9 +9,9 @@
 
 // true;
 // console.log(true);
-let age = 30;
-age = 31;
-const birthYear = 1991;
+// let age = 30;
+// age = 31;
+// const birthYear = 1991;
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -20,6 +20,9 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 const firstName = "Jonas";
 const lastName = "Schmedtmann";
 console.log(firstName + " " + lastName);
-let x = 10 + 5;
-x += 10;
-console.log(x);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+const averageAge = ageJonas + ageSarah / 2;
+console.log(ageJonas, ageSarah, averageAge);
